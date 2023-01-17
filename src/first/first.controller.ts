b84781680c8afe8324cd06217c66fc362a6c4d6b
Afreedi <afreedi@hamon.in>
2023-01-17 16:26:10 +0530

    created postcall and updated status

--- src/first/first.controller.ts
+++ src/first/first.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { IStory } from '../Interface/story';
+import prisma from '../prismaClient';
 @Controller('first')
 export class FirstController {
   @Get()
   secondHello(): string {
     return 'the first sub api in controller';
+  }
+  @Post()
+  async Story(@Body() value): Promise<any> {
+    const { text } = value;
+    const storyText = await prisma.story.create({
+      data: {
+        text: text,
+      },
+    });
+    return storyText;
   }
 }
